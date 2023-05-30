b11076618096d064fe1c0a91ecf8d3ade9b203ff
Matias Rodriguez <mat.rodri@gmail.com>
2023-05-30 01:13:36 -0300

    checking loggers

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,18 +1,30 @@
+import { format } from "path";
 import winston from "winston";
 
 const logger = winston.createLogger({
-    level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
     transports: [
-      new winston.transports.Console(),
-      new winston.transports.File({
-        filename: 'errors.log',
-        level: 'error',
-      }),
+        new winston.transports.Console({
+            levels: {
+                debug: 0,
+                http: 1,
+                info: 2,
+                warning: 3,
+                error: 4,
+                fatal:5,
+            },
+
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            )
+        }),
+    //     new winston.transports.File({
+    //         filename: "errors.log",
+    //         level:"info",
+    //         format: winston.format.simple()
+    //     })
     ],
   });
 
-logger.info('This is an info message');
-logger.warn('This is a warning message');
-logger.error('This is an error message');
 
 export default logger
